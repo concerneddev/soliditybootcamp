4899696ff560b55095daeab327a4befb6c7982cd
Nimish <110283590+concerneddev@users.noreply.github.com>
2024-01-20 17:51:40 +0530

    Create FallbackExample.sol

---
+++ lesson04/FallbackExample.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FallbackExample{
+    uint256 public result;
+
+    receive() external payable {
+        result = 1;
+    }
+
+    fallback() external payable { 
+        result = 2;
+    }
+}
+// Explainer from: https://solidity-by-example.org/fallback/
+// Ether is sent to contract
+//      is msg.data empty?
+//          /   \ 
+//         yes  no
+//         /     \
+//    receive()?  fallback() 
+//     /   \ 
+//   yes   no
+//  /        \
+//receive()  fallback()
